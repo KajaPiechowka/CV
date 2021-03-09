0c5e32b65fc909e4b7d2913ba9e66e3304f7ab31
Kaja Piechówka <kaja.piechowka@gmail.com>
2021-03-09 13:42:59 +0100

    corrections after talk with Tobi

--- cv-en/src/Document.js
+++ cv-en/src/Document.js
@@ -5,7 +5,6 @@ import {
   View,
   Document,
   StyleSheet,
-  Image,
   Font,
   Link,
 } from "@react-pdf/renderer";
@@ -62,7 +61,7 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDidection: "column",
     height: 740,
-    paddingTop: 10,
+    paddingTop: 77,
   },
 
   title: {
@@ -114,10 +113,11 @@ const styles = StyleSheet.create({
   link: {
     fontSize: 11,
     margin: 6,
-
     textDecoration: "none",
     color: "#077DCA",
   },
+
+
 });
 
 // Create Document Component
@@ -126,71 +126,89 @@ const MyDocument = () => (
     <Page style={styles.page}>
       <View style={styles.aside}>
         <Text style={styles.titleLeft}>Personal Data</Text>
-        <Text style={styles.textLeft}>Cracow (mobile location)</Text>
+        <Text style={styles.textLeft}>Cracow (Remote)</Text>
         <Text style={{ ...styles.textLeft, fontWeight: "bold" }}>
           (+48) 698 699 547
         </Text>
         <Text style={styles.textLeft}>kaja.piechowka@gmail.com</Text>
         <Text style={styles.titleLeft}>Technologies</Text>
         <Text style={{ ...styles.textLeft, fontWeight: "bold" }}>
-          JavaScript, React.js, Wordpress
+          JavaScript, React.js, Gatsby.js,
         </Text>
         <Text style={{ ...styles.textLeft, fontWeight: "bold" }}>
-          Gatsby.js, HTML, CSS, Figma.com
+          SASS, Figma.com, GIT, Scrum,
         </Text>
+        <Text style={{ ...styles.textLeft, fontWeight: "bold" }}>
+          Redux, Graphql, Rest API, 
+        </Text>
+        <Text style={{ ...styles.textLeft, fontWeight: "bold" }}>
+           Trello,  Wordpress, Headless CMS 
+        </Text>
+
 
         <Text style={styles.titleLeft}>Projects</Text>
 
-        <Text style={styles.headerLeft}>Acro Game</Text>
+        <Text style={styles.headerLeft}>Acro Game   <Link style={styles.link} src="http://acrogame.net/">
+            Link
+        </Link></Text>
+        <Text style={styles.textLeft}>Project made out of passion for</Text>
+        <Text style={styles.textLeft}>acro yoga and programming. </Text>
         <Text style={{ ...styles.textLeft, fontWeight: "bold" }}>React.js</Text>
-        <Text style={styles.textLeft}>project made with passion for</Text>
-        <Text style={styles.textLeft}>acro yoga and programming </Text>
-        <Link style={styles.link} src="http://acrogame.net/">
-          Website
-        </Link>
-
-        <Text style={styles.headerLeft}>My Shibari Life</Text>
+        
+        <Text style={styles.headerLeft}>Favnote   <Link
+          style={styles.link}
+          src="https://github.com/KajaPiechowka/favnote-app"
+        >
+          Github
+        </Link></Text>
+        <Text style={styles.textLeft}>Eduweb workshop from</Text>
+        <Text style={styles.textLeft}> advanced React.js course.</Text>
         <Text style={{ ...styles.textLeft, fontWeight: "bold" }}>
-          Gatsby.js
+          React.js
         </Text>
-        <Text style={styles.textLeft}>
-          website for shibari school in Cracow
-        </Text>
-        <Text style={styles.textLeft}>project , design , code </Text>
-        <Link
+
+        <Text style={styles.headerLeft}>My Shibari Life   <Link
           style={styles.link}
           src="https://github.com/KajaPiechowka/myShibariLife"
         >
           Github
-        </Link>
-
-        <Text style={styles.headerLeft}>Hatta Website</Text>
+        </Link></Text>
+        <Text style={styles.textLeft}>
+          Website for shibari school in 
+        </Text>
+        <Text style={styles.textLeft}>Cracow. Project , design and code </Text>
+        <Text style={styles.textLeft}>made by myself </Text>
         <Text style={{ ...styles.textLeft, fontWeight: "bold" }}>
           Gatsby.js
         </Text>
-        <Text style={styles.textLeft}>Eduweb workshop</Text>
-        <Link
+        
+
+        <Text style={styles.headerLeft}>Hatta Website   <Link
           style={styles.link}
           src="https://github.com/KajaPiechowka/gatsby-hatta-website"
         >
           Github
-        </Link>
+        </Link></Text>
+        <Text style={styles.textLeft}>Eduweb workshop </Text>
+        <Text style={{ ...styles.textLeft, fontWeight: "bold" }}>
+          Gatsby.js
+        </Text>
+        
 
-        <Text style={styles.headerLeft}>Oddam w Dobre Ręce</Text>
-        <Text style={{ ...styles.textLeft, fontWeight: "bold" }}>React.js</Text>
-        <Text style={styles.textLeft}>CodersLab workshop</Text>
-        <Link
+        <Text style={styles.headerLeft}>Oddam w Dobre Ręce   <Link
           style={styles.link}
           src="https://github.com/KajaPiechowka/oddam-w-dobre-rece"
         >
           Github
-        </Link>
+        </Link></Text>
+        <Text style={styles.textLeft}>CodersLab workshop</Text>
+        <Text style={{ ...styles.textLeft, fontWeight: "bold" }}>React.js</Text>
 
         <Text style={styles.titleLeft}>Languages</Text>
         <Text style={styles.textLeft}>English (intermediate)</Text>
         <Text style={styles.textLeft}>Spanish (beginner)</Text>
         <Text style={styles.titleLeft}>Hobby</Text>
-        <Text style={styles.textLeft}>Joga/Acrojoga, RPG games,</Text>
+        <Text style={styles.textLeft}>Joga, Acro Joga, Tabletop Games,</Text>
         <Text style={styles.textLeft}>Rollerblades, Slackline,</Text>
         <Text style={styles.textLeft}>Rock Music, Painting</Text>
       </View>
@@ -198,66 +216,45 @@ const MyDocument = () => (
         <Text
           style={{
             ...styles.title,
-            marginBottom: 30,
+            marginBottom: 0,
             fontSize: 20,
-            marginTop: 0,
+            marginTop: 10,
             color: "black",
           }}
         >
           Kaja Piechówka
         </Text>
-        <Text
-          style={{
-            ...styles.text,
-
-            fontSize: 10,
-          }}
-        >
-          My last mentor used to say:
-        </Text>
-        <Text
-          style={{
-            ...styles.text,
-            fontWeight: "bold",
-            fontSize: 10,
-          }}
-        >
-          The best code is one that hasn't been written yet. No need to review
-        </Text>
-        <Text
-          style={{
-            ...styles.text,
-            marginBottom: 10,
-            fontWeight: "bold",
-            fontSize: 10,
-          }}
-        >
-          or debug. That's why a good plan and design are paramount!
-        </Text>
+        <Text style={{...styles.text,
+          textTransform: "uppercase", marginBottom: 15, color: "#077DCA"}}>Junior Front-End Software Developer</Text>
 
         <Text style={styles.title}>Courses</Text>
-        <Text style={styles.header}>Wordpress – Eduweb</Text>
-        <Text style={styles.text}>January 2021</Text>
-        <Text style={styles.text}>course of Wordpress from start</Text>
-        <Text style={styles.header}>Gatsby.js – Eduweb</Text>
-        <Text style={styles.text}>October 2020</Text>
-        <Text style={styles.text}>complete course of Gatsby</Text>
+
         <Text style={styles.header}>
           JavaScript Developer: React – Coders Lab
         </Text>
-        <Text style={styles.text}>March 2020 October 2020</Text>
-        <Text style={styles.text}> 434h lessons</Text>
+        <Text style={styles.text}>March 2020 - October 2020</Text>
+        <Text style={styles.text}> 434h of lessons</Text>
         <Text style={styles.text}>
-          Advanced HTML and CSS, javaScript, React course
+          Advanced HTML and CSS, JavaScript, React.js course
         </Text>
+
+        <Text style={styles.header}>Advanced React – Eduweb</Text>
+        <Text style={styles.text}>February 2021</Text>
+        <Text style={styles.text}>Advanced React.js course with Rdux, Context API, </Text>
+        <Text style={styles.text}>Styled Components, JEST Unit Tests</Text>
+
+        <Text style={styles.header}>Gatsby.js – Eduweb</Text>
+        <Text style={styles.text}>October 2020</Text>
+        <Text style={styles.text}>Complete course of Gatsby.js</Text>
+        
         <Text style={styles.header}>Scrum Lab – Coders Lab</Text>
         <Text style={styles.text}>July 2020</Text>
         <Text style={styles.text}>
-          a project aimed at preparing for team work in the Scrum
+          A project aimed at preparing for team work in the Scrum
         </Text>
 
         <Text style={styles.header}>
-          Computer Graphic Course – Szkoła Grafiki Komputerowej
+          Computer Graphics Course – Szkoła Grafiki Komputerowej
         </Text>
         <Text style={styles.text}>October 2017 - January 2018</Text>
         <Text style={styles.text}>
@@ -270,24 +267,24 @@ const MyDocument = () => (
         <Text style={styles.header}>F.H. KAJA-BIS, Cracow – Sales Manager</Text>
         <Text style={styles.text}>December 2015 – Current</Text>
         <Text style={styles.text}>
-          employee management, delivery and ordering of goods,
+          Employee management, delivery and ordering of goods,
         </Text>
         <Text style={styles.text}>
-          customer service, cash register service, goods delivery
+          customer service, cash register service.
         </Text>
 
         <Text style={styles.title}>Education</Text>
         <Text style={styles.header}>Socrates High School, Cracow</Text>
         <Text style={styles.text}>September 2012 – April 2014</Text>
-        <Text style={styles.text}>mathematical and geographical profile</Text>
+        <Text style={styles.text}>Mathematical and geographical profile</Text>
 
         <Text
           style={{
             ...styles.text,
             fontSize: 9,
             positon: "absolute",
-            left: "-180px",
-            bottom: "-40px",
+            left: "-170px",
+            bottom: "-70px",
             width: 550,
             textAlign: "center",
           }}
